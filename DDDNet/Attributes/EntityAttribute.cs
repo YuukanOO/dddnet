8aa6a35e55ea0323e8fd5ae3eca961fef0b6a73b
Julien Leicher <jleicher@gmail.com>
2017-08-28 22:06:59 +0200

    Premiers ajouts !

---
+++ DDDNet/Attributes/EntityAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DDDNet.Attributes
+{
+    /// <summary>
+    /// Représente une entité du domaine.
+    /// 
+    /// Une entité possède une identité unique invariante qui lui est propre. Elle possède une durée de vie
+    /// conséquente et son état change au cours du temps. Son identité lui permet d'être référencée
+    /// par d'autres entités.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public class EntityAttribute : Attribute
+    {
+
+    }
+}
---
+++ DDDNet/Attributes/ValueObjectAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DDDNet.Attributes
+{
+    /// <summary>
+    /// Représente un objet valeur.
+    /// 
+    /// Contrairement aux entités, les objets valeurs ne possède pas d'identité à proprement parler.
+    /// En effet, seuls les attributs permettent de les distinguer entre eux. Ils sont invariants, c'est à dire 
+    /// que leur état ne peut pas être modifié après la création, on préférera alors remplacer la référence tout simplement.
+    /// 
+    /// Les objets valeurs servent à regrouper des notions nécessitant plusieurs propriétés mais formant un tout. Une valeur de
+    /// monnaie par exemple, possèdera à la fois un montant et une devise, l'un ne signifiant rien sans l'autre, il s'agit
+    /// alors d'un objet valeur.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public class ValueObjectAttribute : Attribute
+    {
+
+    }
+}
