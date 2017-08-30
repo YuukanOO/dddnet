561178902c99a21c557237f6811e657a04f8c549
Julien LEICHER <jleicher@gmail.com>
2017-08-30 13:39:44 +0200

    WIP

---
+++ DDDNet/Attributes/FactoryAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DDDNet.Attributes
+{
+    /// <summary>
+    /// Représente une fabrique.
+    /// 
+    /// Les fabriques sont utilisées lorsque la création d'un objet est trop complexe pour être porté par l'objet lui même ou
+    /// que c'est juste impossible sans perdre trop de sens métier.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public class FactoryAttribute : Attribute
+    {
+
+    }
+}
